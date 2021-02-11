9c8195f0c0263d4639f9f814a5583e2a0a8b2e13
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-11 11:45:22 +0100

    explanation

--- js10.js
+++ js10.js
@@ -13,3 +13,10 @@ console.log(person.id);
 console.log(person.lastName);
 console.log(person.fullName());
 
+// JS -- this -- keyword refers to the object it belongs to.
+// In a method, this refers to the owner object.
+// Alone, this refers to the global object.
+// In a function, this refers to the global object.
+// In a function, in strict mode, this is undefined.
+// In an event, this refers to the element that received the event.
+// Methods like call(), and apply() can refer this to any object.
