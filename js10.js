becdd4e033ac25ba7d1bad5dcb10d07ec774734a
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-11 11:44:17 +0100

    this

--- js10.js
+++ js10.js
@@ -1,1 +1,15 @@
 // JavaScript -- this -- keyword
+
+var person = {
+    firstName: "ilia", 
+    lastName: "Doe", 
+    id: 3102, 
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+console.log(person.firstName);
+console.log(person.id);
+console.log(person.lastName);
+console.log(person.fullName());
+
