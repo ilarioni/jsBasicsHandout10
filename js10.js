9c1c2c96660a520f21dd1c84aaa4bf15735ee872
ilarioni <ilia.meshvildishvili@gmail.com>
2021-06-13 11:55:52 +0200

    JSON intro

--- js10.js
+++ js10.js
@@ -67,3 +67,41 @@ console.log(hello1());
 
 // Often, when programming code contains errors, nothing will happen. There are no error messages, and you will get no indications where to search for errors.
 // Searching for (and fixing) errors in programming code is called code debugging.
+
+// JSON - Introduction
+// JSON stands for JavaScript Object Notation. 
+// JSON is a text format for storing and transporting data
+// JSON is "self-describing" and easy to understand
+
+// This example is a JSON string:  '{"name":"John", "age":30, "car":null}'
+
+/*
+It defines an object with 3 properties:
+ - name
+ - age
+ - car
+
+ Each property has a value.
+ If you parse the JSON string with a JavaScript program, you can access the data as an object:
+
+*/
+
+let personName = obj.name;
+let personAge = obj.age;
+
+/*
+What is JSON?
+
+- JSON stands for JavaScript Object Notation
+- JSON is a lightweight data-interchange format
+- JSON is plain text written in JavaScript object notation
+- JSON is used to send data between computers
+- JSON is language independent *
+
+The JSON syntax is derived from JavaScript object notation, but the JSON format is text only.
+Code for reading and generating JSON exists in many programming languages.
+
+The JSON format was originally specified by Douglas Crockford.
+
+*/
+
