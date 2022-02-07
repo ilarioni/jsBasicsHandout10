bc97b5a4d01dc30071a287689b8de5b004fb7dd8
ilarioni mesh <61503627+ilarioni@users.noreply.github.com>
2022-02-07 17:01:24 +0100

    part 1

--- js10.js
+++ js10.js
@@ -267,3 +267,41 @@ function increment() {
     count = count + 1
     countEl.innerText = count
 }
+
+
+// innerText and textContent
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+}
+
+// reset data and start it agai
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+
+
