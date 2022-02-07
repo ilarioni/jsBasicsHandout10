a9b21f9ad767f821c2ad7c6df4884ff83c7318c3
ilarioni mesh <61503627+ilarioni@users.noreply.github.com>
2022-02-07 15:47:24 +0100

    scrimba js

--- js10.js
+++ js10.js
@@ -254,3 +254,16 @@ XML example
 </employees>
 
 */
+
+// Incrementing example  Scrimba
+
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
