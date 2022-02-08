5f7139eb384529748fa4067d68332326edc9c44f
ilarioni mesh <61503627+ilarioni@users.noreply.github.com>
2022-02-08 11:24:46 +0100

    part 2 - black jack

--- js10.js
+++ js10.js
@@ -303,5 +303,19 @@ function save() {
     count = 0
 }
 
+// Scrimba part 2 
+// Blacj jack creation
+
+let firstCard = 10
+let secondCard = 7
+let sum = firstCard + secondCard + 6 // 23
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
 
 
